05db3685505abbc32f38836b33634dd9abd29599
Rama Danadipa Putra Wijaya <ramadanadipa176@gmail.com>
2024-06-27 07:29:18 +0700

    Update  db koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'edukasi_online';
+$dbname = 'b14_36793314_enigma';
 $username = 'root'; // Sesuaikan dengan username MySQL Anda
-$password = ''; // Sesuaikan dengan password MySQL Anda
+$password = 'Thehunter15!'; // Sesuaikan dengan password MySQL Anda
 
 try {
     $koneksi = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
